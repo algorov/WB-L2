d619d56553835a49ed8d75a5f7904c7255a54708
SEMUL <semul.main@icloud.com>
2024-01-25 17:56:08 +0300

    feat + test: dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,4 +1,11 @@
-package main
+package dev02
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
 
 /*
 === Задача на распаковку ===
@@ -18,6 +25,72 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func Unwrap(str string) (string, error) {
+	// Создает билдер, чтобы эффективно конкатенировать строки.
+	builder := strings.Builder{}
 
+	// Перевод полученной строки в слайс рун.
+	runes := []rune(str)
+
+	// Рассматриваемая руна, которая будет множиться в зависимости от коэффециента.
+	var procRune *rune
+
+	// Итерация по срезу.
+	for i := 0; i < len(runes); i++ {
+		// Для удобства отдельная переменная, которая хранит текущую руну.
+		curRune := runes[i]
+
+		// Если нашлелся символ экранирования.
+		if curRune == '\\' {
+			// Проверка, не последний ли этот элемент в слайсе.
+			if i+1 > len(runes)-1 {
+				return "", errors.New("Invalid format")
+			}
+
+			// Добавление экранируемого элемента.
+			builder.WriteRune(runes[i+1])
+			procRune = new(rune)
+			*procRune = runes[i+1]
+
+			// Сдвиг указателя.
+			i++
+
+			// Переход на следующую итерацию.
+			continue
+		}
+
+		// Если текущая руна не цифра.
+		if !unicode.IsDigit(curRune) {
+			// Запись в билдер.
+			builder.WriteRune(curRune)
+
+			// Установка руны как обрабатываемого.
+			procRune = new(rune)
+			*procRune = runes[i]
+		} else {
+			// Если текущая руна - цирфа,
+			// Проверка, что эта руна служит коэффициентом для обрабатываемой руны.
+			if procRune != nil {
+				// Перевод руны в число.
+				count, err := strconv.Atoi(string(curRune))
+				if err != nil {
+					return "", err
+				}
+
+				// Множение руны в N раз.
+				for i := 1; i < count; i++ {
+					builder.WriteRune(*procRune)
+				}
+
+				// Обработанную руну сбрасывает.
+				procRune = nil
+			} else {
+				// Если коэффициент не обабатывает руну, то строка не валидна.
+				return "", errors.New("Invalid format")
+			}
+		}
+	}
+
+	// Сборка строки и возврат.
+	return builder.String(), nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,49 @@
-package main
+package dev02
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// Табличное тестирование.
+func TestUnwrap(t *testing.T) {
+	testCases := []struct {
+		name   string
+		value  string
+		expect string
+	}{
+		{
+			name:   "a4bc2d5e",
+			value:  "a4bc2d5e",
+			expect: "aaaabccddddde",
+		}, {
+			name:   "abcd",
+			value:  "abcd",
+			expect: "abcd",
+		}, {
+			name:   "qwe\\4\\5",
+			value:  "qwe\\4\\5",
+			expect: "qwe45",
+		}, {
+			name:   "qwe\\45",
+			value:  "qwe\\45",
+			expect: "qwe44444",
+		}, {
+			name:   "qwe\\\\5",
+			value:  "qwe\\\\5",
+			expect: "qwe\\\\\\\\\\",
+		}, {
+			name:   "a4bc2d5e\\",
+			value:  "a4bc2d5e\\",
+			expect: "",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			str, _ := Unwrap(testCase.value)
+
+			assert.Equal(t, testCase.expect, str)
+		})
+	}
+}
