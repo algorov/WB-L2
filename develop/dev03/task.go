e22e95ceda010e773bf784ace87df93b552992f4
SEMUL <semul.main@icloud.com>
2024-01-27 16:33:27 +0300

    feat(sort): done

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -5,8 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
 	"sort"
+	"strconv"
 	"strings"
+	"time"
 )
 
 /*
@@ -17,19 +20,19 @@ import (
 
 Поддержать ключи
 
--k — указание колонки для сортировки
--n — сортировать по числовому значению
--r — сортировать в обратном порядке
--u — не выводить повторяющиеся строки
+-k — указание колонки для сортировки [+]
+-n — сортировать по числовому значению [+]
+-r — сортировать в обратном порядке [+]
+-u — не выводить повторяющиеся строки [ ]
 
 Дополнительное
 
 Поддержать ключи
 
--M — сортировать по названию месяца
--b — игнорировать хвостовые пробелы
--c — проверять отсортированы ли данные
--h — сортировать по числовому значению с учётом суффиксов
+-M — сортировать по названию месяца [+]
+-b — игнорировать хвостовые пробелы [ ]
+-c — проверять отсортированы ли данные [+]
+-h — сортировать по числовому значению с учётом суффиксов [+]
 
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
@@ -81,17 +84,83 @@ func main() {
 
 	var compareFunc func(i, j int) bool
 
-	compareFunc = func(i, j int) bool {
-		if r {
-			return getLen(data, i, k) > getLen(data, j, k)
+	// Составляет функцию для сравнивания значений исходя из переданных флагов.
+	switch true {
+	// Если сортировка по числовому значению.
+	case n:
+		compareFunc = func(i, j int) bool {
+			// Парсит строку и пробует перевести в числовое значение.
+			a, _ := strconv.ParseFloat(getElement(data, i, k), 64)
+			b, _ := strconv.ParseFloat(getElement(data, j, k), 64)
+
+			// Если присутствует флаг «r».
+			if r {
+				return a > b
+			}
+
+			return a < b
+		}
+	case h:
+		compareFunc = func(i, j int) bool {
+			// Извлекает из набора отдельные элементы.
+			a, b := getElement(data, i, k), getElement(data, j, k)
+
+			// Пытается найти совпадение по регулярному выражению.
+			valA, errA := extractNumericValue(a)
+			valB, errB := extractNumericValue(b)
+
+			// Проверка на неудачу при поиске.
+			if errA != nil || errB != nil {
+				// Обработка ошибок, например, если извлечение числового значения не удалось
+				// Если присутствует флаг «r».
+				if r {
+					return a > b
+				}
+
+				return a < b
+			}
+
+			// Если присутствует флаг «r».
+			if r {
+				return valA > valB
+			}
+
+			return valA < valB
+		}
+	// Сортировка по названию месяца.
+	case M:
+		compareFunc = func(i, j int) bool {
+			// Если присутствует флаг «r».
+			if r {
+				return parseMonth(getElement(data, j, k)).Before(parseMonth(getElement(data, i, k)))
+			}
+
+			return parseMonth(getElement(data, i, k)).Before(parseMonth(getElement(data, j, k)))
 		}
 
-		return getLen(data, i, k) < getLen(data, j, k)
+	// Если никакого режима сортировки нет, то по умолчанию пойдет обычная сортировка
+	default:
+		compareFunc = func(i, j int) bool {
+			// Если присутствует флаг «r».
+			if r {
+				return getElement(data, i, k) > getElement(data, j, k)
+			}
+
+			return getElement(data, i, k) < getElement(data, j, k)
+		}
 	}
 
-	fmt.Println(data)
+	// Проверка: сортированы ли наши данные?
+	if c {
+		if sort.SliceIsSorted(data, compareFunc) {
+			fmt.Println("Данные отсортированы!")
+		} else {
+			fmt.Println("Данные не отсортированы!")
+		}
+	}
+
+	// Сортировка строк по колонке на основе полученного правила для сортировки.
 	sort.Slice(data, compareFunc)
-	fmt.Println(data)
 
 	// Запись в выходной файл.
 	if err := writeFile(output, data); err != nil {
@@ -154,12 +223,39 @@ func getElement(data [][]string, i, k int) string {
 	return ""
 }
 
-func getLen(data [][]string, i, k int) int {
-	// Если индекс допустим в слайсе, то возвращает элемент.
-	if k >= 0 && k < len(data[i]) {
-		return len(data[i][k])
+// Парсит название месяца из строки.
+func parseMonth(str string) time.Time {
+	if t, err := time.Parse("January", str); err == nil {
+		return t
 	}
 
-	// Иначе возвращает пустую строку.
-	return 0
+	if t, err := time.Parse("Jan", str); err == nil {
+		return t
+	}
+
+	if t, err := time.Parse("1", str); err == nil {
+		return t
+	}
+
+	if t, err := time.Parse("01", str); err == nil {
+		return t
+	}
+
+	// Возвращает значение по умолчанию.
+	return time.Time{}
+}
+
+// Извлекает числовое значение
+func extractNumericValue(s string) (int, error) {
+	// Пытается найти совпадение по регулярному выражению.
+	re := regexp.MustCompile(`(\d+)[KkMm]*`)
+	match := re.FindStringSubmatch(s)
+
+	// Если есть совпадение.
+	if len(match) == 2 {
+		return strconv.Atoi(match[1])
+	}
+
+	// Если нет совпадения.
+	return 0, fmt.Errorf("Не удалось извлечь числовое значение: %s", s)
 }
