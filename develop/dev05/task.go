56f6fb9f2faa88ed341f86aae974b51af87a0826
SEMUL <semul.main@icloud.com>
2024-02-02 19:01:30 +0300

    feat: grep, take 1

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита grep ===
 
@@ -19,5 +27,58 @@ package main
 */
 
 func main() {
+	var v bool
+	var a int
 
+	substring := "пустынном"
+
+	flag.BoolVar(&v, "v", false, "Вместо совпадения, исключать")
+	flag.IntVar(&a, "A", 0, "Печатать +N строк после совпадения")
+
+	flag.Parse()
+
+	data, err := readFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	// Инициализирует правило валидирования строк по умолчанию.
+	var validate func(word, substring string) bool = func(word, substring string) bool {
+		return word == substring
+	}
+
+	// Если активен флаг V, то переопределяем правило отбора.
+	if v {
+		validate = func(word, substring string) bool {
+			return word != substring
+		}
+	}
+
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data[i]); j++ {
+			if validate(data[i][j], substring) {
+				fmt.Println(data[i][j])
+			}
+		}
+	}
+}
+
+// Чтение данных из файла.
+func readFile(path string) (data [][]string, err error) {
+	// Получение файлового дескриптора.
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Сканнер для чтения строки.
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// Добавление в слайс разделенной пробелом строки.
+		data = append(data, strings.Split(scanner.Text(), " "))
+	}
+
+	// Возврат результата.
+	return data, nil
 }
