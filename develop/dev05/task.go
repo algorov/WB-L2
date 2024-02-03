558d1765f41c6ce74a511c7683d5848f7b6e9cb5
SEMUL <semul.main@icloud.com>
2024-02-03 19:31:27 +0300

    feat: grep, take 2

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -14,26 +14,33 @@ import (
 Реализовать утилиту фильтрации (man grep)
 
 Поддержать флаги:
--A - "after" печатать +N строк после совпадения
--B - "before" печатать +N строк до совпадения
+-A - "after" печатать +N строк после совпадения [+]
+-B - "before" печатать +N строк до совпадения [+]
 -C - "context" (A+B) печатать ±N строк вокруг совпадения
--c - "count" (количество строк)
--i - "ignore-case" (игнорировать регистр)
--v - "invert" (вместо совпадения, исключать)
--F - "fixed", точное совпадение со строкой, не паттерн
--n - "line num", печатать номер строки
+-c - "count" (количество строк) [+]
+-i - "ignore-case" (игнорировать регистр) [+]
+-v - "invert" (вместо совпадения, исключать) [+]
+-F - "fixed", точное совпадение со строкой, не паттерн [+]
+-n - "line num", печатать номер строки [+]
 
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
 func main() {
-	var v bool
-	var a int
+	var a, b, context int
+	var c, i, v, f, n bool
 
-	substring := "пустынном"
+	substring := "пустын"
 
-	flag.BoolVar(&v, "v", false, "Вместо совпадения, исключать")
 	flag.IntVar(&a, "A", 0, "Печатать +N строк после совпадения")
+	flag.IntVar(&b, "B", 0, "Печатать -N строк до совпадения")
+	flag.IntVar(&context, "C", 0, "(A+B) печатать ±N строк вокруг совпадения")
+
+	flag.BoolVar(&c, "c", false, "Количество строк")
+	flag.BoolVar(&i, "i", false, "Игнорировать регистр")
+	flag.BoolVar(&v, "v", false, "Вместо совпадения, исключать")
+	flag.BoolVar(&f, "f", false, "Точное совпадение со строкой, не паттерн")
+	flag.BoolVar(&n, "n", false, "Печатать номер строки")
 
 	flag.Parse()
 
@@ -42,24 +49,101 @@ func main() {
 		panic(err)
 	}
 
-	// Инициализирует правило валидирования строк по умолчанию.
-	var validate func(word, substring string) bool = func(word, substring string) bool {
-		return word == substring
+	// Инициализирует правило подготовки данных для последующего анализа.
+	var prepare func(word, substring string) (string, string) = func(word, substring string) (string, string) {
+		return word, substring
+	}
+
+	// Инициализирует правило поиска подстроки в строке.
+	var isContain func(word, substring string) bool = func(word, substring string) bool {
+		return strings.Contains(word, substring)
+	}
+
+	// Инициализирует правило отбора результата по умолчанию.
+	var pass func(isContain bool) bool = func(isContain bool) bool {
+		return isContain
+	}
+
+	// Если активен флаг i, то переопределяем правило подготовки данных.
+	if i {
+		prepare = func(word, substring string) (string, string) {
+			return strings.ToLower(word), strings.ToLower(substring)
+		}
+	}
+
+	// Если активен флаг i, то переопределяем правило поиска.
+	if f {
+		isContain = func(word, substring string) bool {
+			return word == substring
+		}
 	}
 
 	// Если активен флаг V, то переопределяем правило отбора.
 	if v {
-		validate = func(word, substring string) bool {
-			return word != substring
+		pass = func(isContain bool) bool {
+			return !isContain
 		}
 	}
 
+	// Место, где хранятся совпадения на основе полученных правил отбора.
+	result := make([]int, 0, len(data))
+
+	// Применяет правило на данных из входного файла.
 	for i := 0; i < len(data); i++ {
 		for j := 0; j < len(data[i]); j++ {
-			if validate(data[i][j], substring) {
-				fmt.Println(data[i][j])
+			if pass(isContain(prepare(data[i][j], substring))) {
+				result = append(result, i)
 			}
 		}
+	}
+
+	// Обрабротка флага c.
+	if c {
+		fmt.Println("Line count:", len(result))
+	}
+
+	// Если никаких совпадений нет.
+	if len(result) == 0 {
+		fmt.Println("Совпадений нет!")
+
+		return
+	}
+
+	// Нормализация значение флага A.
+	if a < 0 {
+		a = 0
+	}
+
+	// Нормализация значение флага B.
+	if b < 0 {
+		b = 0
+	}
+
+	// Нормализация значение флага С.
+	if context < 0 {
+		context = 0
+	}
+
+	switch true {
+	// Обработка флага C.
+	case context > 0:
+		a, b = context, context
+		printBefore(result[0], b, data)
+		printAfter(result[0], a, data)
+	// Обработка флага A.
+	case a > 0:
+		printAfter(result[0], a, data)
+	// Обработка флага B.
+	case b > 0:
+		printBefore(result[0], b, data)
+	// Обработка флага n.
+	case n:
+		for i, _ := range result {
+			fmt.Printf("[%d]: %s\n", result[i]+1, data[result[i]])
+		}
+	// Печатает первое совпадение, если, конечно, есть.
+	default:
+		fmt.Printf("[%d]: %s\n", result[0]+1, data[result[0]])
 	}
 }
 
@@ -81,4 +165,30 @@ func readFile(path string) (data [][]string, err error) {
 
 	// Возврат результата.
 	return data, nil
+}
+
+// Выводит N записей до совпадения.
+func printBefore(current, count int, data [][]string) {
+	side := current - count
+	// Нормализация границы в пределах допустимых значений индексов
+	if side < 0 {
+		side = 0
+	}
+
+	for index := side; index < current+1; index++ {
+		fmt.Printf("[%d]: %s\n", index+1, data[index])
+	}
+}
+
+// Выводит N записей после совпадения.
+func printAfter(current, count int, data [][]string) {
+	side := current + count
+	// Нормализация границы в пределах допустимых значений индексов
+	if side > len(data) {
+		side = len(data)
+	}
+
+	for index := current; index < side; index++ {
+		fmt.Printf("[%d]: %s\n", index+1, data[index])
+	}
 }
