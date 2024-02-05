2aada26a34808050658b7a6d57068b892c3f56ec
SEMUL <semul.main@icloud.com>
2024-02-05 16:12:32 +0300

    feat: single chan, done

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -34,5 +39,50 @@ fmt.Printf(“fone after %v”, time.Since(start))
 */
 
 func main() {
+	start := time.Now()
 
+	// Возвращает канал, который позже через некоторое количество времени закроется посредством другой горутинки-слушателя.
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	// Ждёт завершения результирующего канала.
+	<-or(
+		sig(2*time.Second),
+		sig(5*time.Second),
+		sig(1*time.Second),
+		sig(1*time.Second),
+		sig(10*time.Second),
+	)
+
+	fmt.Printf("fone after %v", time.Since(start))
+}
+
+// Принимает неопределенное количество каналов и возвращает один канал, который свидетельствует, что все переданные каналы - done.
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	// Если остался один канал, то просто его же возвращает, канал сам закроется после некоторого времени.
+	if len(channels) == 1 {
+		return channels[0]
+	}
+
+	// Результирующий канал, который возвращается.
+	result := make(chan interface{})
+
+	// Горутина, которая обрабатывает закрытые каналы, после обработки, закрывает результирующий канал.
+	go func() {
+		defer close(result)
+		// Ждёт завершения первого канала.
+		<-channels[0]
+
+		// Ждёт завершения других каналов через рекурсию.
+		<-or(channels[1:]...)
+	}()
+
+	// Возвращает результирующий канал.
+	return result
 }
--- develop/dev07/task_test.go
+++ develop/dev07/task_test.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestOr(t *testing.T) {
+	// Начало отсчёта.
+	now := time.Now()
+
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	// Объединение done-каналов.
+	<-or(
+		sig(2*time.Second),
+		sig(5*time.Second),
+		sig(1*time.Second),
+		sig(1*time.Second),
+		sig(7*time.Second),
+	)
+
+	// Фиксация времени после получения результата.
+	after := time.Since(now)
+
+	// Коэффициент расхождения полученного времени от ожидаемого.
+	delta := 100 * (1 - int(after)/int(7*time.Second))
+
+	// assert.
+	assert.Equal(t, 0, delta)
+}
