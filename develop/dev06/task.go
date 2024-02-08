6d981d72c4568939d9e1f319961c9e6bb0b38105
SEMUL <semul.main@icloud.com>
2024-02-08 16:01:41 +0300

    feat: cut, done

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
 
 /*
@@ -28,11 +31,51 @@ func main() {
 
 	// Определение того, что парсить.
 	flag.IntVar(&f, "f", 0, "Выбрать поля (колонки)")
-	flag.StringVar(&d, "d", "", "Использовать другой разделитель")
+	flag.StringVar(&d, "d", "\t", "Использовать другой разделитель")
 	flag.BoolVar(&s, "s", false, "Только строки с разделителем")
 
 	// Парсит.
 	flag.Parse()
 
-	fmt.Println(f, d, s)
+	// Слайс с обработанными данными.
+	data := make([][]string, 0, 10)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Чтобы выйти: 'quit'")
+
+	for {
+		// Сканирует stdin.
+		scanner.Scan()
+		line := scanner.Text()
+
+		// Если пользователь написал стоп-слово, то прекращает считывание.
+		if line == "quit" {
+			break
+		}
+
+		// В остальных случаях кроме, если в строке нет разделителя, но указан флаг, добавляет строку для последующего анализа.
+		if !(s && !strings.Contains(line, d)) {
+			data = append(data, strings.Split(line, d))
+		}
+
+	}
+
+	// Если пользователь не указал флаг или ввёл отрицательное значение, то выводит все введенное пользователем данные c разделителем.
+	if f <= 0 {
+		for _, line := range data {
+			for _, word := range line {
+				fmt.Print(word + d)
+			}
+
+			fmt.Println()
+		}
+	} else {
+		// Если пользователь ввёл валиднкую колонку.
+		for _, line := range data {
+			// Проверка валидности индекса
+			if f < len(line) {
+				fmt.Println(line[f-1])
+			}
+		}
+	}
 }
